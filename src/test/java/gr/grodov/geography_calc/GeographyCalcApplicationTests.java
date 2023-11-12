eaed90b3812f27e3c21e56ae1377f9029758fc16
Grish-Gr <101037462+Grish-Gr@users.noreply.github.com>
2023-11-12 20:32:46 +0500

    Init commit

---
+++ src/test/java/gr/grodov/geography_calc/GeographyCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.grodov.geography_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeographyCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
